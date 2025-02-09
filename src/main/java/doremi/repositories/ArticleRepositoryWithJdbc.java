a6e8babc9e1a61e9a08b219321ace3695d33477a
raclet <raclet@irit.fr>
2025-02-09 22:34:03 +0100

    Resolved #4c : findArticleById de ArticleService

--- src/main/java/doremi/repositories/ArticleRepositoryWithJdbc.java
+++ src/main/java/doremi/repositories/ArticleRepositoryWithJdbc.java
@@ -5,10 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,6 +35,30 @@ public class ArticleRepositoryWithJdbc implements ArticleRepositoryInt {
             throw new RuntimeException("Erreur lors de la récupération des articles", e);
         }
         return articles;
+    }
+
+    public Article findArticleById(int id) {
+        String sql = "SELECT id, title, category FROM articles WHERE id = ?";
+
+        try (Connection connexion = dataSource.getConnection();
+             PreparedStatement statement = connexion.prepareStatement(sql)) {
+
+            statement.setInt(1, id);
+            try (ResultSet fetchedArticle = statement.executeQuery()) {
+                if (fetchedArticle.next()) {
+                    Article article = new Article();
+                    article.setArticleId(fetchedArticle.getInt("id"));
+                    article.setTitle(fetchedArticle.getString("title"));
+                    article.setCategory(fetchedArticle.getString("category"));
+                    return article;
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Erreur lors de la récupération de l'article avec id " + id, e);
+        }
+
+        return null;
     }
 
 }
